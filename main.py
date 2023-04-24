Implementd quaternion tester; fixed quaternion skew-symmetry; integrated plot_transforms into plot_rod_plotly

main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from rod_rate_funcs import rate_func_bvp
-from visual_tools import plot_transforms
+from visual_tools import plot_rod_plotly
 
 import numpy as np
 from scipy.integrate import solve_ivp, solve_bvp
@@ -13,7 +13,7 @@ p_0 = np.array([0, 0, 0])
 R_q_0 = rot.from_euler("xyz", [0, 0, 0], degrees=True).as_quat() # X is up
 
 # Loaded end: gravity load
-n_end = np.array([0, 0, -9.8*0.01])
+n_end = np.array([0, 0, -9.8*0.001])
 m_end = np.array([0, 0, 0])
 
 # Pack the boundry conditions into an evaluation function
@@ -36,5 +36,5 @@ y_0_mesh[0, :] = s_grid
 bvp_soln = solve_bvp(rate_func_bvp, f_bound_conds, s_grid, y_0_mesh)
 
 ## Plot the solution
-fig = plot_transforms(bvp_soln.y[0:3, :], bvp_soln.y[3:7, :])
+fig = plot_rod_plotly(bvp_soln)
 fig.show()

rod_rate_funcs.py
@@ -21,25 +21,19 @@ def mat_skew_sym_3(omega):
 
 def mat_skew_sym_4(omega):
     # Skew symmetric matrix for converting body frame angular velocities to the dq/dt quaternion rate of change
-    # See https://math.stackexchange.com/questions/1797541/can-quaternions-be-used-to-represent-rotation-rate
-    # TODO: Validate which one of these two possible implementations are correct.
+    # See https://math.stackexchange.com/q/1797542 
     x, y, z = omega
     
-    # Omega = np.array([
-    #     [-0, -x, -y, -z],
-    #     [x, -0, z, -y],
-    #     [y, -z, 0, x],
-    #     [z, y, -x, 0]
-    # ])
-    
-    # Correction for SciPy quaternions having the real component at the end:
+    # See https://arxiv.org/pdf/1711.02508.pdf  pg 7, eq 17 and 18.
+    # Take the Right multiplication and substitute in qw=0, qx=omega_x, qy=omega_y, qz=omega_z
+    # Motivation: See pg 21 eq 93. To integrat a body-frame angular velocity in the world frame we must multiply from the right
+    # the orientation by the angular velocity. This is the corresponding matrix to that operation.
     Omega = np.array([
-        [-x, -y, -z, 0],
         [0, z, -y, x],
         [-z, 0, x, y],
-        [y, -x, 0, z]
+        [y, -x, 0, z],
+        [-x, -y, -z, 0]
     ])
-    
     return Omega
 
 # Implement Rucker's equation

test_rod_model.py
@@ -1,7 +1,38 @@
 #!/bin/usr/env python3
+from rod_rate_funcs import mat_skew_sym_4
+
+import numpy as np
 
 def test_mat_skew_sym_4():
-    assert 1==1
+    # Integrate angular velocity for a quaternion state over unit time
+    from scipy.integrate import solve_ivp
+    from scipy.spatial.transform import Rotation as rot
+
+    # Create example ang-vel integration problem
+    # Integrating an angular velocity over unit time is equivalent to treating it as an axis-angle
+    # representation of a rotation, and finding the corresponding quaternion.
+    omega = np.array([1, 2, 3])
+    q_0 = rot.from_euler("xyz", [0, 0, 0])
+    q_end = q_0 * rot.from_rotvec(omega)
+
+    q_0 = q_0.as_quat()
+    q_end = q_end.as_quat()
+
+    # Now numerically integrate it quaternion-style
+    Omega = mat_skew_sym_4(omega)
+    def rate_func(t, q):
+        # Joan Sola's work does not have the 0.5 scale factor...
+        qdot = 0.5 * Omega @ q
+
+        # In theory this should work too? It should be equivalent to our computation here.
+        # qdot = rot.from_quat(q) * rot.from_quat(np.concatenate([omega, [0]]))
+        # qdot = qdot.as_quat()
+        return qdot
+
+    t = [0, 1]
+    ivp_soln = solve_ivp(rate_func, t, q_0, reltol=1-10, abstol=1-10)
+
+    assert np.allclose(ivp_soln.y[:, -1], q_end, atol=1e-3)
 
 def test_batch_matmul():
     assert True